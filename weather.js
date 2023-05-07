7b2b528aa5aa2c90e2cf6a762d2cc471b1cb6bbe
BINIL E B <binilbalan.e.k@gmail.com>
2023-05-07 08:22:35 +0530

    reedited

--- weather.js
+++ weather.js
@@ -1,17 +1,64 @@
 // https://api.openweathermap.org/data/2.5/weather?q=kakkanad&appid=8ac5c4d57ba6a4b3dfcf622700447b1e
+window.addEventListener('load',()=>{
+  fetchData('asia')
+})
 
-document.querySelector('[class="btn btn-light mt-2 f-right"]').addEventListener('click', () => { 
-  placename=place.value
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${placename}&appid=8ac5c4d57ba6a4b3dfcf622700447b1e`)
-  .then((data)=>{
-    return data.json()
-  })
-  .then((weatherData)=>{
-    console.log(weatherData);
-    displayDetails(weatherData)
-  })
+function displaySpinner(state){
+  if(state){
+    document.getElementById("loader").classList.add("spinner-border")
+  }else{
+    document.getElementById("loader").classList.remove("spinner-border")
+  }
+ 
+}
+
+document.querySelector('.btn').addEventListener('click', () => { 
+ let placename = place.value
+ if(placename !== ''){
+  displaySpinner(true)
+  fetchData(placename)
+ }else{
+  document.getElementById("alertmsgsearch").innerHTML = `<p class="text-danger m-0 p-0">No place entered</p>`
+    setTimeout(() => {
+      document.getElementById("alertmsgsearch").innerHTML = null
+    }, 2500);
+  }
   
 })
+
+function fetchData(placename){
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${placename}&appid=8ac5c4d57ba6a4b3dfcf622700447b1e`)
+  .then((response)=>{
+    if (response.ok) {
+      return response.json();
+    }else{
+      setTimeout(() => {
+        displaySpinner(false)
+        document.getElementById("alertmsg").innerHTML = `<div class="alert mb-0 p-2 alert-danger w-50 mt-2 d-flex justify-content-center align-items-center">
+        <span>No weather details available for this place</span> 
+        <i class="fa-solid fa-triangle-exclamation ms-2"></i>
+        </div>`
+      }, 1000);
+      setTimeout(() => {
+        document.getElementById("alertmsg").innerHTML = null
+      }, 2500);
+      throw new Error('Something went wrong')
+    }
+  })
+  .then((weatherData)=>{
+    console.log('weather data',weatherData);
+    setTimeout(() => {
+      displaySpinner(false)
+      displayDetails(weatherData)
+    }, 2000);
+  })
+  .catch(e=>{
+  //   alertmsg.innerHTML=`<div class="alert alert-danger" role="alert">
+  //   error
+  // </div>`
+  console.log('error',e);
+  })
+}
 
 function timeAndDate() {
   let day, amOrpm, hour = new Date().getHours()
@@ -64,10 +111,9 @@ function displayDetails(data) {
     </div>`
 
   contentData = `<div class="tempature">${Math.floor((data.main.temp) - 273.15)}<sup>&#x2103;</sup></div>
-    <div class="place">${data.name}</div>
+    <div class="place">${data.name} <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png" alt=""></div>
     <div class="state">Feel like ${Math.floor((data.main.feels_like) - 273.15)}&#x2103;</div>
-    <div class="date-time">${timeAndDate()}</div>`
-    
+    <div class="date-time">${timeAndDate()}</div>` 
     moreDetail.innerHTML=moreDetails
     displayContent.innerHTML=contentData
 } 
